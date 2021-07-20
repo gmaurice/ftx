a32736d36d5faa36f49f1b3e4fb2bc8ca856713b
gmaurice <gmaurice@localhost>
2021-07-20 21:13:32 +0000

    activate ping as mentioned at https://docs.ftx.com/#request-process

--- websocket/websocket_manager.py
+++ websocket/websocket_manager.py
@@ -1,9 +1,8 @@
-import json
-import time
+import json, time, logging
 from threading import Thread, Lock
 
 from websocket import WebSocketApp
-
+import traceback
 
 class WebsocketManager:
     _CONNECT_TIMEOUT_S = 5
@@ -27,14 +26,14 @@ class WebsocketManager:
 
     def _connect(self):
         assert not self.ws, "ws should be closed before attempting to connect"
-
+        logging.info('Connecting to ' + self._get_url() + '...')
         self.ws = WebSocketApp(
             self._get_url(),
             on_message=self._wrap_callback(self._on_message),
             on_close=self._wrap_callback(self._on_close),
             on_error=self._wrap_callback(self._on_error),
+            on_pong=self._on_pong,
         )
-
         wst = Thread(target=self._run_websocket, args=(self.ws,))
         wst.daemon = True
         wst.start()
@@ -47,18 +46,23 @@ class WebsocketManager:
                 return
             time.sleep(0.1)
 
+        # ws_ping = Thread(target=self._heartbeat)
+        # ws_ping.daemon = True
+        # ws_ping.start()
+
     def _wrap_callback(self, f):
         def wrapped_f(ws, *args, **kwargs):
             if ws is self.ws:
                 try:
                     f(ws, *args, **kwargs)
                 except Exception as e:
+                    traceback.print_exc()
                     raise Exception(f'Error running websocket callback: {e}')
         return wrapped_f
 
     def _run_websocket(self, ws):
         try:
-            ws.run_forever()
+            ws.run_forever(ping_interval=15, ping_payload='{"op":"ping"}')
         except Exception as e:
             raise Exception(f'Unexpected error while running websocket: {e}')
         finally:
@@ -80,10 +84,15 @@ class WebsocketManager:
                 if self.ws:
                     return
 
+    def _on_pong(self, ws, message):
+        return
+
     def _on_close(self, ws):
+        logging.warn('Closed connection, reconnecting...')
         self._reconnect(ws)
 
     def _on_error(self, ws, error):
+        logging.error(str(error))
         self._reconnect(ws)
 
     def reconnect(self) -> None:
